6710b91114140151a4914386762146a160cc19ba
Maxim Alov <Alov.M.D@sberbank.ru>
2021-06-07 14:12:13 +0300

    Update version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.5.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
-    testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    implementation(libs.appCompat)
+    implementation(libs.coreKtx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junitAndroidExt)
+    androidTestImplementation(libs.espresso)
 }
