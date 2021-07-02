18748d1a78852ae8fb3c0d38c2744cf75504b56f
Maxim Alov <Alov.M.D@sberbank.ru>
2021-07-02 21:47:31 +0300

    Upgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-beta04")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
     }
 }
 
