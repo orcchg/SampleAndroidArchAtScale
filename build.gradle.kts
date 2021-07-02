736db74ef21b3652de11978d23fb7a4ef739c827
Maxim Alov <Alov.M.D@sberbank.ru>
2021-07-02 22:00:54 +0300

    Reference version catalog's entry while adding AGP and KtGP to build script's classpath (use workaround until Gradle 7.2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta04")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        // TODO(m.alov): issuee https://github.com/gradle/gradle/issues/15383
+        // TODO(m.alov): issue https://github.com/gradle/gradle/issues/16958
+        val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs") as org.gradle.accessors.dm.LibrariesForLibs
+
+        classpath(libs.androidGradlePlugin)
+        classpath(libs.kotlinPlugin)
     }
 }
 
