90bad4617004a85ddb7296da8db6b4339e67066a
orcchg <orcchg@gmail.com>
2022-05-25 21:58:15 +0400

    Upgrade build tools: AGP and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        // TODO(m.alov): issuee https://github.com/gradle/gradle/issues/15383
-        // TODO(m.alov): issue https://github.com/gradle/gradle/issues/16958
-        val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs") as org.gradle.accessors.dm.LibrariesForLibs
-
         classpath(libs.androidGradlePlugin)
         classpath(libs.kotlinPlugin)
     }
